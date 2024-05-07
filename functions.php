103789c5445e0000bc177c4f585220403564db17
UDAYVEERUS <udayveerus348567@gmail.com>
2024-05-07 23:25:53 +0530

    Add some more functions

--- functions.php
+++ functions.php
@@ -19,5 +19,56 @@ echo "Total numebr of marks of uday $sumMarks / 500  <br>";
 echo "Total numebr of marks of diksha  $sumMarksHistory / 500 <br>";
 
 
+// calculate sum
+function calculateSum(){
+    $a = 5;
+    $b = 999;
 
+    $sum = $a + $b;
+    echo "Total sum of A and B = $sum <br>"; 
+}
+calculateSum();
+
+// increamenr function 
+
+// call by value
+function incr($i)  
+{  
+    $i++;  
+}  
+$i = 5;  
+incr($i);  
+echo $i; 
+echo "<br>";
+
+
+// call by reference 
+
+function increment($i)  
+{  
+    $i++;  
+}  
+$i = 5;  
+increment($i);  
+echo $i; 
+echo "<br>";
+
+// Default Argument Values
+function Hello($name = "Udayveer"){
+    echo "Hello $name <br>";
+}
+Hello("Diksha");
+Hello(); //passing no value  
+Hello("Mona");
+
+// Variable Length Argument
+function add(...$nums){
+    $sum = 0;
+
+    foreach($nums as $n){
+        $sum = $sum + $n;
+    }
+    return $sum;
+}
+echo add(1,2,3,4,5,18,4697,29,2874,762,2399,36);  // output 10830
 ?>
