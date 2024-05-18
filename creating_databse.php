d97abbb3d1133d7195207b8ea73872b21ade4e9e
UDAYVEERUS <udayveerus348567@gmail.com>
2024-05-18 12:35:28 +0530

    Creating databse

---
+++ creating_databse.php
@@ -0,0 +1,28 @@
+<?php
+
+// echo "Connecting to database <br>";
+
+$server_name = "localhost";
+$user_name = "root";
+$pass = "";
+
+$conn = mysqli_connect($server_name, $user_name, $pass);
+
+if(!$conn){
+ die("Database not connected");
+}
+else{
+    echo "Database connected successfully<br>";
+}
+
+$sql = "CREATE DATABASE cwhdb";
+$result = mysqli_query($conn, $sql);
+
+if($result){
+    echo "The Database created successfully! <br>";
+}
+else{
+    echo "The database was not created  ------>> " .mysqli_error($conn);
+}
+
+?>
