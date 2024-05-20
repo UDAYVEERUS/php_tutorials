77703937311ee619da19694faddcf2620ffdb855
UDAYVEERUS <udayveerus348567@gmail.com>
2024-05-20 23:03:51 +0530

    minor change

--- select_data.php
+++ select_data.php
@@ -16,16 +16,22 @@ if(!$conn){
 $sql = "SELECT * from `contacts`";
 $result = mysqli_query($conn, $sql);
 
+// find how many rows in the table
 $num = mysqli_num_rows($result);
 echo "Total number of record found in contact table  " . $num;
 echo "<br>";
+$number = 1;
 if($num > 0){
     // $row = mysqli_fetch_assoc($result);
     // echo var_dump($row);
     while($row = mysqli_fetch_assoc($result)){
-        echo $row['serial number'] . " Hello ".$row['name'] . " Welcome to " .$row['Date'];
+        echo $number . ": Hello ".$row['name'] . " Your Email is " .$row['email'];
         echo "<br>";
+        $number = $number + 1;
     }
+}
+else{
+    echo "NO RECORD FOUND";
 }
 
 ?>
